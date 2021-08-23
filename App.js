f3eebab8d27954c05578331254d5dd9d62d9b069
asaba <swift0910@cpoint-lab.co.jp>
2021-08-23 14:23:59 +0900

    react-navigation追加

--- App.js
+++ App.js
@@ -1,37 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
-import MapView from 'react-native-maps';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './Home';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const Stack = createNativeStackNavigator();
     return (
-      <View style={styles.container}>
-        <MapView
-        initialRegion={{
-          latitude: 34.78825,
-          longitude: 134.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        style={styles.map} 
-      />
-      <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home">
+            {props => <Home {...props} />}
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   map: {
     width: "100%",
     height: "100%",
+  },
+  button: {
+    elevation: Platform.OS === 'android' ? 10 : 0,
+    position: 'absolute',
+    top: 160,
+    right: 10,
   },
 });
 
