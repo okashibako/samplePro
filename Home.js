3adb73d69658ae91dcac492023148e107b153a82
asaba <swift0910@cpoint-lab.co.jp>
2021-08-24 10:51:39 +0900

    useState導入

--- Home.js
+++ Home.js
@@ -1,29 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
 import { Button } from "native-base"
 
-class Home extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        <View style={styles.button} >
-          <Button onPress={() => console.log("hello world")}>PRIMARY</Button>
-        </View>
-        <MapView
-          initialRegion={{
-            latitude: 34.78825,
-            longitude: 137.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-          style={styles.map} 
-        />
+function Home() {
+  const [region, setRegion] = useState(
+    {
+      latitude: 34.78825,
+      longitude: 137.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+  );
+
+  useEffect(() => {
+    for(var i = 0; i < 3; i++){
+      console.log(region)
+    }
+  });
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.button} >
+        <Button onPress={() => console.log("hello world")}>PRIMARY</Button>
       </View>
-    );
-  }
+      <MapView
+        region={region}
+        style={styles.map}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
